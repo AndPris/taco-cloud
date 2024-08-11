ba99438082b83ed863beb9afb4b40c506ee6071c
Andrii <andpris527@gmail.com>
2024-08-11 21:08:23 +0300

    Initial commit

---
+++ src/main/java/sia/tacocloud/Taco.java
@@ -0,0 +1,23 @@
+package sia.tacocloud;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class Taco {
+    private Long id;
+    private Date createdAt;
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @NotNull(message="You must choose at least 1 ingredient")
+//    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
+}
