8e045d2bcf999779196e64e5c8f03fd13b7297a0
Andrii <andpris527@gmail.com>
2024-08-13 11:39:36 +0300

    Add user reference to Order

--- src/main/java/sia/tacocloud/web/OrderController.java
+++ src/main/java/sia/tacocloud/web/OrderController.java
@@ -3,6 +3,7 @@ package sia.tacocloud.web;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.Order;
+import sia.tacocloud.User;
 import sia.tacocloud.data.OrderRepository;
+import sia.tacocloud.data.UserRepository;
+
+import java.security.Principal;
 
 @Slf4j
 @Controller
@@ -20,10 +25,12 @@ import sia.tacocloud.data.OrderRepository;
 @SessionAttributes("order")
 public class OrderController {
     private final OrderRepository orderRepo;
+    private final UserRepository userRepository;
 
     @Autowired
-    public OrderController(final OrderRepository orderRepo) {
+    public OrderController(final OrderRepository orderRepo, UserRepository userRepository) {
         this.orderRepo = orderRepo;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/current")
@@ -32,11 +39,13 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if(errors.hasErrors()) {
             return "orderForm";
         }
 
+        order.setUser(user);
         orderRepo.save(order);
         sessionStatus.setComplete();
 
