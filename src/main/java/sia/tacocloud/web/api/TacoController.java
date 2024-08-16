d462ea3258460058a6f4acab5b5ab5c0f5244df6
Andrii <andpris527@gmail.com>
2024-08-16 11:43:43 +0300

    Add GET, POST, PUT, PATCH and DELETE methods

--- src/main/java/sia/tacocloud/web/api/TacoController.java
+++ src/main/java/sia/tacocloud/web/api/TacoController.java
@@ -1,13 +1,17 @@
 package sia.tacocloud.web.api;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sia.tacocloud.Order;
 import sia.tacocloud.Taco;
+import sia.tacocloud.data.OrderRepository;
 import sia.tacocloud.data.TacoRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/tacos",
@@ -15,9 +19,11 @@ import sia.tacocloud.data.TacoRepository;
 @CrossOrigin(origins = "http://tacocloud:8080")
 public class TacoController {
     private final TacoRepository tacoRepo;
+    private final OrderRepository orderRepo;
 
-    public TacoController(TacoRepository tacoRepo) {
+    public TacoController(TacoRepository tacoRepo, OrderRepository orderRepo) {
         this.tacoRepo = tacoRepo;
+        this.orderRepo = orderRepo;
     }
 
     @GetMapping(params = "resent")
@@ -25,5 +31,66 @@ public class TacoController {
         PageRequest page = PageRequest.of(
                 0, 12, Sort.by("createdAt").descending());
         return tacoRepo.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco  = tacoRepo.findById(id);
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
+    @PutMapping(path = "/{orderId}", consumes = "application/json")
+    public Order putOrder(@PathVariable("orderId") Long orderId, @RequestBody Order order) {
+        order.setId(orderId);
+        return orderRepo.save(order);
+    }
+
+    @PatchMapping(path = "/{orderId}", consumes = "application/json")
+    public Order patchOrder(@PathVariable("orderId") Long orderId, @RequestBody Order patch) {
+        Order order = orderRepo.findById(orderId).get();
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+
+        return orderRepo.save(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable("orderId") Long orderId) {
+        try {
+            orderRepo.deleteById(orderId);
+        } catch (EmptyResultDataAccessException e) {}
     }
 }
